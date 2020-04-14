2efca94999b5bb1b6826c3ffcb1220b0bb3d6d45
Pitt Wu <kgb00128@gmail.com>
2020-04-14 16:19:56 +0800

    [ADD] 首頁進行 layout 排版，採用測試 api 渲染內容

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  rules: {
+    'prefer-const': 0
+  }
+}
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,12 @@
 <template lang="pug">
-  div.home
+  main.home__wrap
+    article.article__wrap
+      .article__list(v-for="node in articleData")
+        .title
+          h3 {{ node.title }}
+          i {{ node.date }}
+        .summary {{ node.content | summary }}
+    aside aside
 </template>
 
 <script>
@@ -9,7 +16,33 @@ export default {
   },
   data () {
     return {
-      formData: ''
+      articleData: ''
+    }
+  },
+
+  created () {
+    this.getArticle()
+  },
+
+  methods: {
+    getArticle () {
+      let url = `${process.env.VUE_APP_ARTICLE_API}`
+      // fetch api 不僅是原生語法，同時預設採用 promise 包裹
+      fetch(url).then((res) => {
+        // 回傳的檔案若為 JSON 格式，需要解析處理
+        return res.json()
+      }).then((result) => {
+        console.log(result)
+        this.articleData = result.data
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  },
+
+  filters: {
+    summary: (val) => {
+      return val.substring(0, 200)
     }
   }
 }
