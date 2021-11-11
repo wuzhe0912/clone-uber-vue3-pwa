10863b83819ddd2657ef435901148e06ac7cc729
Pitt Wu <kgb00128@gmail.com>
2021-11-11 18:01:06 +0800

    feat: setting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
+function App() {
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
