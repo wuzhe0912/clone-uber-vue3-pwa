f83e5f3c7ca89c276faee80252097ced488c6bc0
Pitt Wu <kgb00128@gmail.com>
2022-01-11 11:14:33 +0800

    chore: init typescript template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from 'App.module.css';
+
+function App() {
+  return (
+    <div className={style.app}>
+      <h1>Hello React with TypeScript</h1>
+    </div>
+  );
+}
+
+export default App;
